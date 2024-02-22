be9702cb098e836d81ac8b5824a9b58f22122734
Sai Peram <sai11.peram@gmail.com>
2024-02-21 21:22:24 -0600

    hello!

--- speakwise/lib/screens/home/home.dart
+++ speakwise/lib/screens/home/home.dart
@@ -4,6 +4,8 @@ class Home extends StatelessWidget {
   const Home({super.key}); 
 
   // This is a comment for our project
+  // We are trying to build an app!
+  // New Comment!
  
   @override
   Widget build(BuildContext context) {

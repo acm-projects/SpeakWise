a0688a73d6e6c26bc6f0ea110e678bbc14c3739d
Sai Peram <sai11.peram@gmail.com>
2024-02-21 21:14:44 -0600

    here's my first commit!

--- speakwise/lib/screens/home/home.dart
+++ speakwise/lib/screens/home/home.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  const Home({super.key}); 
 
+  // This is a comment for our project
+ 
   @override
   Widget build(BuildContext context) {
     return Container(

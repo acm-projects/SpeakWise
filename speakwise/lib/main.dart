a4df1a36637630e2c057c7f0598b535a4cdeb451
Sai Peram <sai11.peram@gmail.com>
2024-03-13 16:59:26 -0500

    test

--- speakwise/lib/main.dart
+++ speakwise/lib/main.dart
@@ -1,25 +1,25 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:speakwise/constants/colors.dart';
+import 'package:speakwise/screens/InterviewAnalysisScreen.dart';
+import 'package:speakwise/screens/InterviewQuestions_screen.dart';
+import 'package:speakwise/screens/LoginOrCreateAccount_screen.dart';
+import 'package:speakwise/screens/LoginPage.dart';
+import 'package:speakwise/screens/ProfilePage.dart';
+import 'package:speakwise/screens/Record_Screen.dart';
+import 'package:speakwise/screens/StartUp_page.dart';
 import 'package:speakwise/screens/StartUp_page.dart';
 import 'package:speakwise/screens/createAccount/createAccountPage.dart';
-import 'package:speakwise/screens/LoginPage.dart';
 import 'package:speakwise/screens/homePage.dart';
-import 'package:speakwise/constants/colors.dart';
 import 'package:speakwise/screens/questionCard/QuestionPage.dart';
-
-import 'package:speakwise/screens/Record_Screen.dart';
-import 'package:speakwise/screens/InterviewAnalysisScreen.dart';
-import 'package:speakwise/screens/LoginOrCreateAccount_screen.dart';
-import 'package:speakwise/screens/InterviewQuestions_screen.dart';
-
-import 'package:speakwise/screens/StartUp_page.dart';
-import 'package:speakwise/screens/ProfilePage.dart';
 
 void main() {
   runApp(const MyApp()); 
 }
+
+// This comment is a test comment
 
  class MyApp extends StatelessWidget {
   const MyApp({super.key});

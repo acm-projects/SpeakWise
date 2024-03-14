1730e57d48edfed3701c6dd7b2082024308683bf
Sai Peram <sai11.peram@gmail.com>
2024-03-13 20:29:03 -0500

    committing wrapper.dart

--- speakwise/lib/screens/wrapper.dart
+++ speakwise/lib/screens/wrapper.dart
@@ -1,6 +1,8 @@
 import "package:flutter/material.dart";
-import "package:speakwise/screens/authenticate/authenticate.dart";
-import "package:speakwise/screens/home/home.dart";
+import "package:provider/provider.dart";
+import 'package:speakwise/models/usermodel.dart';
+import 'package:speakwise/screens/authenticate/authenticate.dart';
+import 'package:speakwise/screens/home/home.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
@@ -8,7 +10,17 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     
+    final user = Provider.of<UserTag>(context);
+    
     //return either Home or Authenticate Widget 
+
+  if (user == Null) {
     return Authenticate();
+  } else {
+    return Home();
+
+  }
+
+    
   }
 }

1fc1375e1c925d5d465407121486f39c6503c470
Sai Peram <sai11.peram@gmail.com>
2024-03-13 20:25:30 -0500

    committing sign_in.dart

--- speakwise/lib/screens/authenticate/sign_in.dart
+++ speakwise/lib/screens/authenticate/sign_in.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:speakwise/services/auth.dart';
+
 class SignIn extends StatefulWidget{
+  const SignIn({super.key});
+
 
   @override
+  // ignore: library_private_types_in_public_api
   _SignInState createState() => _SignInState();
 }
 
@@ -15,9 +19,23 @@ class _SignInState extends State<SignIn> {
       appBar: AppBar(
       backgroundColor: Colors.orange[400],
       elevation: 0.0,
-    
-
+      title: const Text('Sign in to SpeakWise')
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+        child: ElevatedButton(
+          child: const Text('Sign in'),
+          onPressed: () async{
+            dynamic result = await _auth.signInAnon();
+            if (result == null) {
+              print('error signing in');
+            } else {
+              print('signed in');
+              print(result.uid);
+            }
+          },
       )
+    )
     );
   }
 }

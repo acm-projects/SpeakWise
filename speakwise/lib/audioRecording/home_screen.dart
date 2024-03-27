751ba432290437cef250d67ee5a4a23ad5e95f73
swayam528 <107274953+swayam528@users.noreply.github.com>
2024-03-27 12:00:42 -0500

    Made some revisions to speech to text feature removing extra buttons from the screen

--- speakwise/lib/audioRecording/home_screen.dart
+++ speakwise/lib/audioRecording/home_screen.dart
@@ -2,76 +2,70 @@ import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:speech_to_text/speech_recognition_result.dart';
 import 'package:speech_to_text/speech_to_text.dart';
-class HomeScreen extends StatefulWidget
-{
-  const HomeScreen({super.key});
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
-class _HomeScreenState extends State<HomeScreen>
-{
-  TextEditingController userInputTextEditingController = TextEditingController();
+
+class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController userInputTextEditingController =
+      TextEditingController();
   final SpeechToText speechToTextInstance = SpeechToText();
   String recordedAudioString = "";
   bool isLoading = false;
-  void initializeSpeechToText() async
-  {
+
+  void initializeSpeechToText() async {
     print("Initialize speech to text");
     await speechToTextInstance.initialize();
     print("speech to text initialized");
-    setState(() {
-    });
+    setState(() {});
   }
-  void startListeningNow() async
-  {
+
+  void startListeningNow() async {
     FocusScope.of(context).unfocus();
     await speechToTextInstance.listen(onResult: onSpeechToTextResult);
-    setState(() {
-    });
+    setState(() {});
   }
-  void stopListeningNow() async
-  {
+
+  void stopListeningNow() async {
     await speechToTextInstance.stop();
+    setState(() {});
+  }
+
+  void onSpeechToTextResult(SpeechRecognitionResult recognitionResult) {
+    print("Speech Result: ${recognitionResult.recognizedWords}");
     setState(() {
+      recordedAudioString = recognitionResult.recognizedWords;
     });
   }
- void onSpeechToTextResult(SpeechRecognitionResult recognitionResult) {
-  print("Speech Result: ${recognitionResult.recognizedWords}");
-  setState(() {
-    recordedAudioString = recognitionResult.recognizedWords;
-  });
-}
+
   @override
   void initState() {
     super.initState();
     initializeSpeechToText();
   }
+
   @override
-  Widget build(BuildContext context)
-  {
+  Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.white,
-        onPressed: ()
-        {
-        },
+        onPressed: () {},
         child: Padding(
           padding: const EdgeInsets.all(4.0),
-          child: Image.asset(
-            "images/sound.png"
-          ),
+          child: Image.asset("images/sound.png"),
         ),
       ),
       appBar: AppBar(
         flexibleSpace: Container(
           decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.purpleAccent.shade100,
-                Colors.deepPurple,
-              ]
-            )
-          ),
+              gradient: LinearGradient(colors: [
+            Colors.purpleAccent.shade100,
+            Colors.deepPurple,
+          ])),
         ),
         title: Image.asset(
           "images/logo.png",
@@ -79,113 +73,50 @@ class _HomeScreenState extends State<HomeScreen>
         ),
         titleSpacing: 10,
         elevation: 2,
-        actions: [
-          //chat
-          Padding(
-            padding: const EdgeInsets.only(right: 4, top: 4),
-            child: InkWell(
-              onTap: ()
-              {
-              },
-              child: const Icon(
-                Icons.chat,
-                size: 40,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          //image
-          Padding(
-            padding: const EdgeInsets.only(right: 8, left: 4),
-            child: InkWell(
-              onTap: ()
-              {
-              },
-              child: const Icon(
-                Icons.image,
-                size: 40,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ],
       ),
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(20),
           child: Column(
             children: [
-              const SizedBox(height: 40,),
+              const SizedBox(
+                height: 40,
+              ),
               //image
               Center(
                 child: InkWell(
-                  onTap: ()
-                  {
+                  onTap: () {
                     speechToTextInstance.isListening
                         ? stopListeningNow()
                         : startListeningNow();
                   },
                   child: speechToTextInstance.isListening
-                      ? Center(child: LoadingAnimationWidget.beat(
-                          size: 300,
-                          color: speechToTextInstance.isListening
-                              ? Colors.deepPurple
-                              : isLoading
-                              ? Colors.deepPurple[400]!
-                              : Colors.deepPurple[200]!,
-                        ),)
+                      ? Center(
+                          child: LoadingAnimationWidget.beat(
+                            size: 300,
+                            color: speechToTextInstance.isListening
+                                ? Colors.deepPurple
+                                : isLoading
+                                    ? Colors.deepPurple[400]!
+                                    : Colors.deepPurple[200]!,
+                          ),
+                        )
                       : Image.asset(
-                    "images/assistant_icon.png",
-                    height: 300,
-                    width: 300,
-                  ),
+                          "images/assistant_icon.png",
+                          height: 300,
+                          width: 300,
+                        ),
                 ),
               ),
               const SizedBox(
                 height: 50,
               ),
-              //text field with a button
-              Row(
-                children: [
-                  //text field
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 4.0),
-                      child: TextField(
-                        controller: userInputTextEditingController,
-                        decoration: const InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: "how can i help you?",
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(width: 10,),
-                  //button
-                  InkWell(
-                    onTap: ()
-                    {
-                      print("send user input");
-                    },
-                    child: AnimatedContainer(
-                      padding: const EdgeInsets.all(15),
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.rectangle,
-                        color: Colors.deepPurpleAccent
-                      ),
-                      duration: const Duration(
-                        milliseconds: 1000,
-                      ),
-                      curve: Curves.bounceInOut,
-                      child: const Icon(
-                        Icons.send,
-                        color: Colors.white,
-                        size: 30,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              // Display recorded text
+              if (recordedAudioString.isNotEmpty)
+                Text(
+                  recordedAudioString,
+                  style: TextStyle(fontSize: 18),
+                ),
             ],
           ),
         ),
